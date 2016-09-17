9c89e8b2292eaa7fee5e4df7dfef1a804393b0c1
Lee Avital <leeavital@gmail.com>
2016-09-17 16:25:06 -0400

    Add submitReducer

---
+++ src/submitReducer.ts
@@ -0,0 +1,31 @@
+import { initialState, IState } from "./reducer";
+import { IAction } from "./actions";
+import { shallowCopy } from "./shallowCopy";
+
+export function submitTodoReducer(state: IState = initialState, action: IAction) {
+  switch(action.type) {
+    case "submitTodo":
+      let copy = shallowCopy(state);
+      copy.todos = shallowCopy(copy.todos);
+      
+      let errors = [];
+      if (action.todo.name == null || action.todo.name === "") {
+        //TODO: check facets
+        errors.push("name must not be empty");
+      }
+
+      if (errors.length > 0) {
+          copy.todos.createErrors = errors;   
+          return copy;
+      }
+
+      // past this point, input is okay and we can change the state
+      copy.todos.todos = copy.todos.todos.concat(action.todo);
+      copy.isCreating = false;
+      copy.todos.createErrors = [];
+      return copy;
+
+    default:
+      return state;
+  }
+}
